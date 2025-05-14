d8956c0ddd1313666d37a918b7f9445f33db549d
PraveenUdara <praveen17vck@gmail.com>
2025-05-14 22:51:41 +0530

    commit-02-usercontroller

--- LastMinutePantry-Project/project/backend/controllers/userController.js
+++ LastMinutePantry-Project/project/backend/controllers/userController.js
@@ -9,3 +9,16 @@ const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
