e29aec9f5cb36db2638c4c37319d004aaadab93e
HashiniJ <it22171924@my.sliit.lk>
2025-05-15 01:47:42 +0530

    commit-02-delivery

--- LastMinutePantry-Project/project/backend/controllers/deliveryController.js
+++ LastMinutePantry-Project/project/backend/controllers/deliveryController.js
@@ -1,1 +1,27 @@
 import express from express;
+import Delivery from '../models/deliveryModel.js';
+
+// @desc    Create new delivery
+// @route   POST /api/deliveries
+// @access  Private
+const createDelivery = asyncHandler(async (req, res) => {
+  const {
+    orderId,
+    address,
+    preferredTime
+  } = req.body;
+
+  const delivery = await Delivery.create({
+    order: orderId,
+    customer: req.user._id,
+    address,
+    preferredTime
+  });
+
+  if (delivery) {
+    res.status(201).json(delivery);
+  } else {
+    res.status(400);
+    throw new Error('Invalid delivery data');
+  }
+});
