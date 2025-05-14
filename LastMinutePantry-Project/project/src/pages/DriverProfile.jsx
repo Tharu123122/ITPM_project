98a58e3f829fe9d6e31df209980272363a13a33d
HashiniJ <it22171924@my.sliit.lk>
2025-05-15 02:33:33 +0530

    commit-75-deliveries

--- LastMinutePantry-Project/project/src/pages/DriverProfile.jsx
+++ LastMinutePantry-Project/project/src/pages/DriverProfile.jsx
@@ -116,3 +116,31 @@ export function DriverProfile() {
     logout();
     navigate('/role-selection');
   };
+
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="bg-white rounded-lg shadow-md overflow-hidden">
+          {/* Profile Header */}
+          <div className="relative h-32 bg-orange-600">
+            <div className="absolute -bottom-16 left-8">
+              <div className="relative">
+                <img
+                  src={formData.profileImage}
+                  alt={formData.name}
+                  className="w-32 h-32 rounded-full border-4 border-white object-cover"
+                />
+                {isEditing && (
+                  <label className="absolute bottom-0 right-0 p-2 bg-white rounded-full shadow-md cursor-pointer hover:bg-gray-50">
+                    <Camera className="h-5 w-5 text-gray-600" />
+                    <input
+                      type="file"
+                      className="hidden"
+                      accept="image/*"
+                      onChange={handleFileChange}
+                    />
+                  </label>
+                )}
+              </div>
+            </div>
+          </div>
