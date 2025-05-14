2fcbd58a34121183674897ed74c5bbc1aee85829
PraveenUdara <praveen17vck@gmail.com>
2025-05-14 23:23:44 +0530

    commit-10-usercontroller

--- LastMinutePantry-Project/project/backend/controllers/userController.js
+++ LastMinutePantry-Project/project/backend/controllers/userController.js
@@ -128,3 +128,10 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
+// @desc    Get all users (admin only)
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.json(users);
+  });
