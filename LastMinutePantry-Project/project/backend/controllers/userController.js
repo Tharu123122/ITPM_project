3a5ba6cfde818fae702a868ef25918a86daa4a8e
PraveenUdara <praveen17vck@gmail.com>
2025-05-14 22:58:09 +0530

    commit-04-usercontroller

--- LastMinutePantry-Project/project/backend/controllers/userController.js
+++ LastMinutePantry-Project/project/backend/controllers/userController.js
@@ -34,3 +34,23 @@ const registerUser = asyncHandler(async (req, res) => {
       res.status(400);
       throw new Error('User already exists');
     }
+    const user = await User.create({
+        email,
+        password,
+        role,
+        ...otherFields,
+      });
+    
+      if (user) {
+        res.status(201).json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+          token: generateToken(user._id),
+        });
+      } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+      }
+    });
