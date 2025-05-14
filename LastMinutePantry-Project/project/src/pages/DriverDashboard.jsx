6d272c90f477961671d31b2cb1bf164c52595201
HashiniJ <it22171924@my.sliit.lk>
2025-05-15 02:22:08 +0530

    commit-50-deliveries

--- LastMinutePantry-Project/project/src/pages/DriverDashboard.jsx
+++ LastMinutePantry-Project/project/src/pages/DriverDashboard.jsx
@@ -10,3 +10,14 @@ export function DriverDashboard() {
     activeHours: 0,
     totalDistance: 0
   });
+
+  useEffect(() => {
+    // Fetch driver stats
+    // This would be replaced with actual API call in production
+    setStats({
+      totalDeliveries: 8,
+      earnings: 145.50,
+      activeHours: 6.5,
+      totalDistance: 45
+    });
+  }, []);
