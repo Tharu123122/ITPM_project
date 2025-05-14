8ea88a015fd435124a6b8c82d5b1b7a5d59de328
PraveenUdara <praveen17vck@gmail.com>
2025-05-14 22:49:16 +0530

    commit1-01-usercontroller

---
+++ LastMinutePantry-Project/project/backend/controllers/userController.js
@@ -0,0 +1,11 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
+
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
