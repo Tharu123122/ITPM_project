fb81abf186300216f98e752c35e6f7bb4fef2767
PraveenUdara <praveen17vck@gmail.com>
2025-05-14 23:30:45 +0530

    commit-13-adminMIddleware

--- LastMinutePantry-Project/project/backend/middleware/adminMiddleware.js
+++ LastMinutePantry-Project/project/backend/middleware/adminMiddleware.js
@@ -1,1 +1,12 @@
-//adminMiddleware
+import asyncHandler from 'express-async-handler';
+
+const admin = asyncHandler(async (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an admin');
+  }
+});
+
+export { admin };
