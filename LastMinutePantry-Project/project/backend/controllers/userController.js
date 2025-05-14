c9bde6e7133b31963089f0150dbf1c5690610a71
PraveenUdara <praveen17vck@gmail.com>
2025-05-14 23:12:08 +0530

    commit-07-usercontroller

--- LastMinutePantry-Project/project/backend/controllers/userController.js
+++ LastMinutePantry-Project/project/backend/controllers/userController.js
@@ -93,3 +93,11 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
