1919045516f08b265e5bf965f6098aab17801622
PraveenUdara <praveen17vck@gmail.com>
2025-05-14 23:34:45 +0530

    commit-14-usermodel

---
+++ LastMinutePantry-Project/project/backend/models/userModel.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ['customer', 'vendor', 'driver', 'admin'],
